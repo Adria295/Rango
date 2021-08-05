4968f2399c54ac6351277382a63566ec1675df1a
“Wenlan <903271709@qq.com>
2021-08-05 10:50:08 +0800

    completed registration verification.

--- static/javascript/registration_verification.js
+++ static/javascript/registration_verification.js
@@ -1,3 +1,5 @@
+var flag = true;
+
 function checkUsername() {
     let uPattern = /^[a-zA-Z0-9_@.+--]{4,30}$/;
     let userName = $("#id_username").val();
@@ -7,9 +9,68 @@ function checkUsername() {
     }else {
         $("#userInfo").text("Unavailable Username")
         $("#userInfo").css("color","red")
+        flag = false;
     }
 }
 
-function test(){
-    alert("test");
+function checkEmail() {
+    let email = $("#id_email").val();
+    let reg = new RegExp("^[a-zA-Z0-9-_]+@[a-zA-Z0-9-_]+([.][a-zA-Z0-9-_]+)+$");
+    if(reg.test(email)) {
+        $("#userEmail").text("")
+        $("#userEmail").css("color","black")
+    } else {
+        $("#userEmail").text("The email address you entered is wrong, please check!")
+        $("#userEmail").css("color","red")
+        flag = false;
+    }
 }
+
+function checkPsw() {
+    let password = $("#id_password1").val();
+
+    if(password.length >= 6) {
+        $("#userPsw1").text("")
+        $("#userPsw1").css("color","black")
+    } else {
+        $("#userPsw1").text("The password must be at least 6 characters long")
+        $("#userPsw1").css("color","red")
+        flag = false;
+    }
+}
+
+function checkRepsw() {
+    let password = $("#id_password2").val();
+
+    if(password.length >= 6) {
+        $("#userPsw2").text("")
+        $("#userPsw2").css("color","black")
+    } else {
+        $("#userPsw2").text("The password must be at least 6 characters long")
+        $("#userPsw2").css("color","red")
+        // If the password length verification fails, the subsequent logic does not need to be executed again
+        flag = false;
+        return;
+    }
+    // 比对两次密码
+    let psw = $("#id_password1").val();
+    if(psw == password) {
+        $("#userPsw2").text("")
+        $("#userPsw2").css("color","black")
+    } else {
+        $("#userPsw2").text("The two passwords are inconsistent, please check!")
+        $("#userPsw2").css("color","red")
+        flag = false;
+    }
+}
+
+function checkForm(){
+    flag = true;
+    checkUsername();
+    checkEmail();
+    checkPsw();
+    checkPsw();
+
+    return flag;
+}
+
